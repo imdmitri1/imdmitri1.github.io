eac8a6791c2653b1f1c456652ffed251cb4ce399
Dmitri Stefanisin <email.ds@protonmail.com>
2018-01-05 22:38:04 -0600

    add raw code to auto adjust content height, need to fix

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// // $(window).resize(function(){
+// var height = $("body").height()
+// console.log(height)
+// // $('.first, .third').height(height / 2);
+// // //alert(height);
+// // })
+// // $(window).resize();
+// //
+// // window.onload = function() {
+// //   console.log("happening")
+// //   $(window).resize();
+// //
+// // }
+//
+// window.onclick = function quarter() {
+//   console.log("click")
+//   $('.first, .third').height(height / 2);
+//   // window.resizeTo(
+//   //   window.screen.availWidth / 2,
+//   //   window.screen.availHeight / 2
+//   // );
+// }
